7168ec7c5df90effc7f25f0548bc5e4e28ddb147
tkhs-0213 <tkhs19990213usg@icloud.comtkhs19990213usg@icloud.com>
2019-10-21 16:09:09 +0900

    ajax clear2

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -33,7 +33,7 @@ $(function(){
     .done(function(message){
       var html = buildHTML(message);
       $('.messages').append(html)
-      $('.form__message').val('');
+      $("#new_message")[0].reset();
       $('.form__submit').prop('disabled', false);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     })
