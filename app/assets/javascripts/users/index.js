ea06411decfa4242223d807c2740d28a6e6c0b5d
tkhs-0213 <tkhs19990213usg@icloud.comtkhs19990213usg@icloud.com>
2019-10-23 21:07:18 +0900

    inkl clear

---
+++ app/assets/javascripts/users/index.js
@@ -0,0 +1,62 @@
+$(function() {
+
+  function appendUser(user){
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+              </div>`;
+  $("#user-search-result").append(html);               
+  }
+  function appendNoUser(user){
+  var html =  `<div class='chat-group-user clearfix'>
+                  <p class="chat-group-user__name">${ user }</p></div>`
+  $("#user-search-result").append(html);
+  }
+  function  addUser(id,name){
+    var html = `
+            <div class='chat-group-user'>
+              <input name='group[user_ids][]' type='hidden' value='${id}'> 
+              <p class='chat-group-user__name'>${name}</p>
+              <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+            </div>`
+    $("#chat-group-users").append(html)
+  }
+
+  $('#user-search-field').on("keyup", function(e) {
+    e.preventDefault();
+    var input = $('#user-search-field').val()
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users) {
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+      else {
+        appendNoUser("一致するユーザはいません");
+      }
+    })
+    .fail(function() {
+      alert('error');
+    })
+  
+    $("#user-search-result").on('click', ".user-search-add", function(e) {
+      e.preventDefault();
+      $(this).parent().remove();
+      var id = $(this).data('user-id');
+      var name = $(this).data('user-name');
+       addUser(id,name);
+    });
+    $("#chat-group-users").on("click", ".user-search-remove", function(e){
+      e.preventDefault();
+      $(this).parent().remove();
+    });
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
