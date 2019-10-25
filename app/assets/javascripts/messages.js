ecf9e72209927e3fe731744db6fa81341380a4db
tkhs-0213 <tkhs19990213usg@icloud.comtkhs19990213usg@icloud.com>
2019-10-25 15:36:03 +0900

    clear2

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,41 +1,41 @@
 $(function(){
   var buildMessageHTML = function(message) {
-    // if (message.content && message.image.url) {
-    //   var html =`<div class="message" data-message-id='${message.id}'>
-    //                 <div class="upper-message">
-    //                   <div class="upper-message__user-name">
-    //                     ${ message.user_name }
-    //                   </div>
-    //                   <div class="upper-message__date">
-    //                     ${ message.time }
-    //                   </div>
-    //                 </div>
-    //                 <div class="lower-message">
-    //                   <div class="lower-message__content">
-    //                       ${ message.content }    
-    //                   </div>
-    //                   <div class="lower-message__image">
-    //                     <img src='${ message.image.url }'
-    //                   </div>
-    //                 </div>
-    //             　</div>`
-    // } else if (message.content) {
-    //   var html =`<div class="message" data-message-id='${message.id}'>
-    //                 <div class="upper-message">
-    //                   <div class="upper-message__user-name">
-    //                     ${ message.user_name }
-    //                   </div>
-    //                   <div class="upper-message__date">
-    //                     ${ message.time }
-    //                   </div>
-    //                 </div>
-    //                 <div class="lower-message">
-    //                   <div class="lower-message__content">
-    //                       ${ message.content }    
-    //                   </div>
-    //                 </div>
-    //           　  </div>`
-    // }else if (message.image.url) {
+    if (message.content && message.image.url) {
+      var html =`<div class="message" data-message-id='${message.id}'>
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${ message.user_name }
+                      </div>
+                      <div class="upper-message__date">
+                        ${ message.time }
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <div class="lower-message__content">
+                          ${ message.content }    
+                      </div>
+                      <div class="lower-message__image">
+                        <img src='${ message.image.url }'
+                      </div>
+                    </div>
+                　</div>`
+    } else if (message.content) {
+      var html =`<div class="message" data-message-id='${message.id}'>
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${ message.user_name }
+                      </div>
+                      <div class="upper-message__date">
+                        ${ message.time }
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <div class="lower-message__content">
+                          ${ message.content }    
+                      </div>
+                    </div>
+              　  </div>`
+    }else if (message.image.url) {
       
       var html =`<div class="message" data-message-id='${message.id}'>
                     <div class="upper-message">
@@ -55,39 +55,33 @@ $(function(){
                       </div>
                     </div>
                   </div>`
-    // };
+    };
     return html;
   };
-  // function scrollBottom(){
-  //   var target = $('.message').last();
-  //   var position = target.offset().top + $('.messages').scrollTop();
-  //   $('.messages').animate({
-  //     scrollTop: position
-  //   }, 300, 'swing');
-  // }
-  // var reloadMessages = function() {
-    
-  //   last_message_id = $('.message:last').data('id');
-  //   $.ajax({
-  //     url: "api/messages#index",
-  //     type: 'Get',
-  //     dataType: 'json',
-  //     data: {id: last_message_id}
-  //   })
 
-  //   .done(function(messages) {
-  //     var html ='';
-  //     messages.forEach(function(message) {
-  //       html = buildMessageHTML(message);
-  //     $(".messages").append(html);
-  //     scrollBottom();
-  //     })
-  //   })
-  //   .fail(function() {
-  //     aleat('自動更新に失敗しました');
-  //   });
-  // };
-  
+  var reloadMessages = function() {
+    
+    last_message_id = $('.message:last').data('id');
+    $.ajax({
+      url: "api/messages#index",
+      type: 'Get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+
+    .done(function(messages) {
+      var html ='';
+      messages.forEach(function(message) {
+        html = buildMessageHTML(message);
+      $(".messages").append(html);
+      scrollBottom();
+      })
+    })
+    .fail(function() {
+      aleat('自動更新に失敗しました');
+    });
+  };
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -105,14 +99,11 @@ $(function(){
       $('.messages').append(html)
       $("#new_message")[0].reset();
       $('.form__submit').prop('disabled', false);
-      scrollBottom();
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     })
     .fail(function(){
       alert('error');
     })
-    .always(function(message){
-      $('.send').prop('disabled', false);
-    })
   })
-  setInterval(reloadMessages, 5000);
+   setInterval(reloadMessages, 5000);
 });
