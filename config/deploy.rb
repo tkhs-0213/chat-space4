a862fdbd4a112021c6992c24bcda6acfcf81462e
tkhs-0213 <tkhs19990213usg@icloud.comtkhs19990213usg@icloud.com>
2019-10-28 15:16:25 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: [~/.ssh/ChatSpace2]  
+                  keys: ~/.ssh/ChatSpace2.pem
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
