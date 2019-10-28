45fae6cccb251705c552dedffcb8dbd852dad563
tkhs-0213 <tkhs19990213usg@icloud.comtkhs19990213usg@icloud.com>
2019-10-28 15:03:20 +0900

    deploy2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,17 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.2"
 
-set :application, "chat-space4""
-set :repo_url, "git@github.com:tkhs-0213/chat-space4.git"
+set :application, "chat-space4"
+set :repo_url,  "git@github.com:tkhs-0213/chat-space4.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 2.5.1'#カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1'
 
-# どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['ChatSpace2.pem']  ※例：~/.ssh/key_pem.pem
+                  keys: [~/.ssh/ChatSpace2]  
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
